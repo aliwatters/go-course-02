ac353a867cead4608043bb26532cdcfeaf14173b
Ali Watters <ali.watters@gmail.com>
2016-01-09 08:14:56 -0800

    reorg

---
+++ chp01/dup1.go
@@ -0,0 +1,25 @@
+// Dup1 prints the text of each line that appears more than
+// once in standard input, preceeded by it's count
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+
+	// Note: ignoring errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
